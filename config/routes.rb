c985459bf64b99859125afe243e51df1fff5c079
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2021-11-16 14:29:45 +0900

    写真挿入

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  resources :comments
+  devise_for :users
+  root to: "home#index"
+  resources :users
+  resources :rooms
+  get 'reservation/new' # 入力画面
+  post 'reservation/confirm' # 確認画面
+  post 'reservation/back' # 確認画面から「入力画面に戻る」をクリックしたとき
+  post 'reservation/complete' # 完了画面
+
+end
